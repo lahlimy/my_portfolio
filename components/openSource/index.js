a06a861d11e200b8504839502f0e5120f2df9e9d
aziz <aziz.lahlimy@gmail.com>
2023-02-06 10:00:51 +0100

    new updates

--- components/openSource/index.js
+++ components/openSource/index.js
@@ -1,3 +1,4 @@
+import { data } from 'jquery';
 import React from 'react';
 import OpenSourceRepo from './repo';
 
@@ -8,50 +9,50 @@ const repoDummy = {
 const repoDummies = [repoDummy, repoDummy, repoDummy, repoDummy, repoDummy, repoDummy];
 
 class OpenSource extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            loaded: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+    };
+  }
 
-    async componentDidMount() {
-        const res = await fetch('https://api-my-rebo.onrender.com/api/courses');
-        const json = await res.json();
+  async componentDidMount() {
+    const res = await fetch("https://api-my-rebo.onrender.com/api/courses");
+    const json = await res.json();
 
-        this.setState({
-            data: json.data.repos,
-            loaded: true
-        });
-    }
+    this.setState({
+      data: json.data.repos,
+      loaded: true,
+    });
+  }
 
+  render() {
+    const { loaded, data } = this.state;
+    const repos = loaded ? data : repoDummies;
 
-    render() {
-        const { loaded, data } = this.state;
-        const repos = loaded ? data : repoDummies;
+    return (
+      <section className="section-open_source">
+        <div className="xl:container xl:mx-auto nav-container">
+          <h2>Open source</h2>
 
-        return <section className='section-open_source'>
-            <div className='xl:container xl:mx-auto nav-container'>
-                <h2>Open source</h2>
-
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
-                    {
-                        repos.map((repo, i) =>
-                            <OpenSourceRepo
-                                key={`repo ${i}`}
-                                href={repo.url}
-                                name={repo.name}
-                                des={repo.description}
-                                lang={repo.language}
-                                stars={repo.stars}
-                                commits={repo.commits}
-                            />)
-                    }
-                </div>
-            </div>
-        </section >
-    }
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {repos.map((repo, i) => (
+              <OpenSourceRepo
+                key={`repo ${i}`}
+                href={repo.url}
+                name={repo.name}
+                des={repo.description}
+                lang={repo.language}
+                stars={repo.stars}
+                commits={repo.commits}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default OpenSource;
--- components/timelines/index.js
+++ components/timelines/index.js
@@ -1,47 +1,76 @@
-import React from 'react';
-import Timeline from './timeline';
+import React from "react";
+import Timeline from "./timeline";
 
 function Timelines() {
-    return <section className='section-timelines'>
-        <div className='xl:container xl:mx-auto section-timelines-container'>
-            <h2>Work experience</h2>
+  return (
+    <section className="section-timelines">
+      <div className="xl:container xl:mx-auto section-timelines-container">
+        <h2>Work experience</h2>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-[10px]'>
-                <Timeline duration='May 2021 &#8212; Present' title='Web Developer Developer' company='Upwork' >
-                    <ul>
-                        <li>&#x2022;  Translate requirements into complicated but clean and efficient code.</li>
-                        <li>&#x2022;  Develop schedules and determine milestones.</li>
-                        <li>&#x2022;  Polish the game, maintain code, fix bugs, and iron out occurring problems.</li>
-                    </ul>
-                </Timeline>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10px]">
+          <Timeline
+            duration="2021 &#8212; Present(2 years)"
+            title="Full-stack Developer"
+            company="WHITESOFT"
+          >
+            <ul>
+              <li>
+                &#x2022; Created site layout and user interface using REACTJS
+                and Material UI practices.
+              </li>
+              <li>
+                &#x2022; Built and styled 3+ new mobile-friendly websites and
+                transitioned the old site into a mobile-friendly version0
+              </li>
+              <li>
+                &#x2022; Developed code fixes and enhancements for inclusion in
+                future code releases and patches.
+              </li>
+            </ul>
+          </Timeline>
 
-                <Timeline duration='Jun 2020 &#8212; Oct 2021' title='Unity Game Developer' company='Aiwa Holding' >
-                    <ul>
-                        <li>&#x2022;  Building a simulator iOS/Android game from the ground up to finish a game.</li>
-                        <li>&#x2022;  Implementation of features using flexible and salable programming patterns.</li>
-                        <li>&#x2022;  Integration to user Authentication and Database.</li>
-                    </ul>
-                </Timeline>
+          <Timeline
+            duration="Apr 2020 &#8212; Dec 2020"
+            title="Full-stack Developer"
+            company="AIA"
+          >
+            <ul>
+              <li>
+                &#x2022; Analyzed requirements and designed, developed, and
+                implemented software applications for multiple websites.
+              </li>
+              <li>
+                &#x2022; Evaluated potential software products based on new and
+                existing system development and migration requirements.
+              </li>
+              <li>
+                &#x2022; Provided continued maintenance and development of bug
+                fixes and patch sets for existing web applications
+              </li>
+            </ul>
+          </Timeline>
 
-                <Timeline duration='Jul 2020 &#8212; Jun 2021' title='Unity Game Developer' company='Applift'>
-                    <ul>
-                        <li>&#x2022;  Responsible for developing hyper-casual games prototypes.</li>
-                        <li>&#x2022;  Produce prototypes of gameplay ideas and features.</li>
-                        <li>&#x2022;  Core gameplay systems and UI/UX.</li>
-                    </ul>
-                </Timeline>
-
-                <Timeline duration='Mar 2020 &#8212; Dec 2020' title='Unity Game Developer' company='Leela Games'  >
-                    <p>Responsible for different aspects.</p>
-                    <ul>
-                        <li>&#x2022;  Mesh generation gameplay mechanics.</li>
-                        <li>&#x2022;  Core gameplay systems and UI/UX.</li>
-                        <li>&#x2022;  Level design.</li>
-                    </ul>
-                </Timeline>
-            </div>
+          <Timeline
+            duration="Mar 2019 &#8212; 2020 (1year)"
+            title="Web Developer"
+            company="UPWORK"
+          >
+            <ul>
+              <li>
+                &#x2022; Translate requirements into complicated but clean and
+                efficient code.
+              </li>
+              <li>&#x2022; Develop schedules and determine milestones.</li>
+              <li>
+                &#x2022; Polish the game, maintain code, fix bugs, and iron out
+                occurring problems.
+              </li>
+            </ul>
+          </Timeline>
         </div>
+      </div>
     </section>
+  );
 }
 
 export default Timelines;
